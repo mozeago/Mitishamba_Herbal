6b540ee25ad6d31b44b63d1f5ee5430ef4d79ae2
Moses Asiago <mozzeago@gmail.com>
2022-02-17 22:16:53 +0300

    designed horizontal scrollable cards using LazyRow

--- app/src/main/java/com/mozanafrica/mitishambaherbal/MainActivity.kt
+++ app/src/main/java/com/mozanafrica/mitishambaherbal/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mozanafrica.mitishambaherbal.ui.theme.MitishambaHerbalTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,8 +32,51 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
+@Composable
+fun PreviewFunctions() {
+    HerbList()
+}
+
 @Composable
 fun HerbList() {
+    HerbCardLazyColumn()
+}
+
+@Composable
+fun HerbListLazyColumn() {
+    val list = (0..5).map { it.toString() }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        items(items = list, itemContent = { text_changed ->
+            Text(text = text_changed, fontSize = 50.sp, fontStyle = FontStyle.Italic )
+            HerbListLazyColumn()
+        })
+
+    }
+}
+
+@Composable
+fun HerbCardLazyColumn() {
+    val list = (0..10).map { it.toString() }
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(Color.Gray)
+
+    ) {
+        items(items = list, itemContent = { item ->
+            HerbSingleCard()
+        })
+    }
+}
+
+@Composable
+fun HerbSingleCard() {
     val paddingTenDp = Modifier.padding(10.dp)
     Card(elevation = 10.dp, modifier = paddingTenDp.fillMaxWidth()) {
         Column(modifier = paddingTenDp) {
