8dce32a2bbcae12bfeecce030c1c8f317ab96bfc
Moses Asiago <mozzeago@gmail.com>
2022-02-23 21:00:25 +0300

    designed cards sizes, headings and body typography

--- app/src/main/java/com/mozanafrica/mitishambaherbal/MainActivity.kt
+++ app/src/main/java/com/mozanafrica/mitishambaherbal/MainActivity.kt
@@ -8,12 +8,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,10 +52,15 @@ fun HerbListLazyColumn() {
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(10.dp)
             .wrapContentHeight()
     ) {
         items(items = list, itemContent = { text_changed ->
-            Text(text = "Title $text_changed", fontSize = 50.sp, fontStyle = FontStyle.Italic)
+            Text(
+                text = "Title $text_changed",
+                fontSize = 24.sp,
+                color = MaterialTheme.colors.secondaryVariant
+            )
             HerbCardLazyRow()
         })
 
@@ -63,6 +73,7 @@ fun HerbCardLazyRow() {
     LazyRow(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(10.dp)
             .wrapContentHeight()
 
     ) {
@@ -72,19 +83,44 @@ fun HerbCardLazyRow() {
     }
 }
 
+@Preview
 @Composable
 fun HerbSingleCard() {
+    val robotoFontFamily = FontFamily(
+        Font(R.font.roboto_bold, FontWeight.Bold)
+    )
     val paddingTenDp = Modifier.padding(10.dp)
-    Card(elevation = 10.dp, modifier = paddingTenDp.fillMaxWidth()) {
+    Card(elevation = 10.dp, modifier = paddingTenDp.width(300.dp)) {
         Column(modifier = paddingTenDp) {
             Image(
                 painter = painterResource(id = R.drawable.mitishambaherbal),
                 contentDescription = "Herb name will got here",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
             )
             Spacer(modifier = Modifier.padding(top = 10.dp))
-            Text(text = "Herb Name Goes here")
+            Text(
+                text = "Herb Name Goes here",
+                fontFamily =
+                FontFamily(
+                    Font(R.font.roboto_bold)
+                ),
+                fontWeight = FontWeight.Bold, style = MaterialTheme.typography.subtitle1,
+            )
             Spacer(modifier = Modifier.padding(top = 10.dp))
-            Text(text = "Herb subtitle goes here")
+            Text(
+                text = "Herb subtitle goes here",
+                fontFamily =
+                FontFamily(
+                    Font(R.font.roboto_bold)
+                ),
+                style = MaterialTheme.typography.subtitle2,
+            )
+            Text(
+                text = "n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.",
+                style = MaterialTheme.typography.body1
+            )
         }
     }
 }
