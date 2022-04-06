e631f902f5b8b9353d9af9d985cddd08970fa792
Moses Asiago <mozzeago@gmail.com>
2022-04-06 21:09:17 +0300

    added searchview on sticky header,made the list categories dynamic from enum,edited size of Cards

--- app/src/main/java/com/mozanafrica/mitishambaherbal/MainActivity.kt
+++ app/src/main/java/com/mozanafrica/mitishambaherbal/MainActivity.kt
@@ -8,13 +8,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -23,6 +23,8 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mozanafrica.mitishambaherbal.presentation.ui.tags.TagNames
+import com.mozanafrica.mitishambaherbal.presentation.ui.tags.getAllTags
 import com.mozanafrica.mitishambaherbal.ui.theme.MitishambaHerbalTheme
 import com.mozanafrica.mitishambaherbal.ui.theme.Shapes
 import com.mozanafrica.mitishambaherbal.ui.theme.grey_light
@@ -50,6 +52,7 @@ fun HerbList() {
 }
 
 @OptIn(ExperimentalFoundationApi::class)
+@Preview
 @Composable
 fun HerbListLazyColumn() {
     val list = (0..5).map { it.toString() }
@@ -59,36 +62,56 @@ fun HerbListLazyColumn() {
             .wrapContentHeight()
     ) {
         stickyHeader {
-            Text(
-                text = "Search input and chips",
-                color = Color.Gray,
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .padding(5.dp)
-                    .fillMaxWidth()
-            )
+            Surface(
+                modifier = Modifier.fillMaxWidth(),
+                elevation = 8.dp
+            ) {
+                Column() {
+                    Row() {
+                        TextField(value = "Search Here", onValueChange = { searchVslue -> })
+                    }
+                    ScrollableTabRow(
+                        selectedTabIndex = 1,
+                        modifier = Modifier
+                            .background(MaterialTheme.colors.surface)
+                            .fillMaxWidth()
+                    ) {
+                        for (tag in getAllTags()) {
+                            Text(
+                                text = tag.value,
+                                style = MaterialTheme.typography.body2,
+                                modifier = Modifier.padding(8.dp)
+                            )
+                        }
+
+                    }
+                }
+            }
 
 
         }
         items(items = list, itemContent = { text_changed ->
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .padding(15.dp, 0.dp, 0.dp, 0.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(
-                    text = "Title $text_changed",
-                    modifier = Modifier.padding(15.dp),
-                    style = MaterialTheme.typography.h1
-                )
-                Icon(
-                    painterResource(id = R.drawable.ic_baseline_arrow_forward_24),
-                    contentDescription = "",
-                )
+            for (tag in getAllTags()) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .padding(15.dp, 0.dp, 0.dp, 0.dp)
+                        .fillMaxWidth()
+                ) {
+
+                    Text(
+                        text = " ${tag.value}",
+                        modifier = Modifier.padding(15.dp),
+                        style = MaterialTheme.typography.h1,
+                    )
+                    Icon(
+                        painterResource(id = R.drawable.ic_baseline_arrow_forward_24),
+                        contentDescription = "",
+                    )
+                }
+                HerbCardLazyRow()
             }
-            HerbCardLazyRow()
         })
 
     }
@@ -113,10 +136,10 @@ fun HerbCardLazyRow() {
 fun HerbSingleCard() {
     val paddingTenDp = Modifier.padding(10.dp)
     Card(
-        elevation = 10.dp,
+        elevation = 8.dp,
         modifier = Modifier
             .padding(15.dp)
-            .fillMaxWidth()
+            .width(300.dp)
             .clickable { },
         shape = MaterialTheme.shapes.medium,
         backgroundColor = MaterialTheme.colors.surface,
@@ -125,7 +148,11 @@ fun HerbSingleCard() {
         ) {
         Column(modifier = paddingTenDp) {
             Image(
-                painter = painterResource(id = R.drawable.mitishambaherbal),
+                painter = painterResource(id = R.drawable.kivumbasi),
+                modifier = Modifier
+                    .align(CenterHorizontally)
+                    .size(100.dp)
+                    .clip(CircleShape),
                 contentDescription = "Herb name will got here",
             )
             Spacer(modifier = Modifier.padding(top = 10.dp))
